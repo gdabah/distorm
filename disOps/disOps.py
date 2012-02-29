096a95276a161e973fcd5988be61dd4c5f0a12d4
distorm <distorm@d69571ce-fa96-11de-930f-d36749347e7b>
2012-02-29 12:29:24 +0000

    Fixed a typo in AVX pseudo compare instruction.

--- disOps/disOps.py
+++ disOps/disOps.py
@@ -55,7 +55,7 @@ idsCounter = len("undefined") + 2 # Starts immediately after this one.
 
 SSECmpTypes = ["EQ", "LT", "LE", "UNORD", "NEQ", "NLT", "NLE", "ORD"]
 AVXCmpTypes = ["EQ", "LT", "LE", "UNORD", "NEQ", "NLT", "NLE", "ORD",
-				"EQ_UQ", "NGE", "NGT", "FALSE", "EQ_OQ", "GE", "GT", "TRUE",
+				"EQ_UQ", "NGE", "NGT", "FALSE", "NEQ_OQ", "GE", "GT", "TRUE",
 				"EQ_OS", "LT_OQ", "LE_OQ", "UNORD_S", "NEQ_US", "NLT_UQ", "NLE_UQ", "ORD_S",
 				"EQ_US", "NGE_UQ", "NGT_UQ", "FALSE_OS", "NEQ_OS", "GE_OQ", "GT_OQ", "TRUE_US"]
 
