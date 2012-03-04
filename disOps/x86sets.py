50c22be258dff094cea0d2425f61d6f70d84e23e
distorm <distorm@d69571ce-fa96-11de-930f-d36749347e7b>
2012-03-04 19:59:54 +0000

    Added XSAVE64/XSAVEOPT64/XRSTOR64 and similar instructions. Fixed a problem with MOVD/MOVQ. Fixed typos in a few instructions.

--- disOps/x86sets.py
+++ disOps/x86sets.py
@@ -115,17 +115,19 @@ class Instructions:
 		Set("0f, ab", ["BTS"], [OPT.RM_FULL, OPT.REG_FULL], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag.PRE_LOCK)
 		Set("0f, ac", ["SHRD"], [OPT.RM_FULL, OPT.REG_FULL, OPT.IMM8], IFlag.MODRM_REQUIRED | IFlag._32BITS)
 		Set("0f, ad", ["SHRD"], [OPT.RM_FULL, OPT.REG_FULL, OPT.REGCL], IFlag.MODRM_REQUIRED | IFlag._32BITS)
-		Set("0f, ae /00", ["FXSAVE"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS)
-		Set("0f, ae /01", ["FXRSTOR"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS)
+		Set("0f, ae /00", ["FXSAVE", "", "FXSAVE64"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag._64BITS | IFlag.PRE_REX | IFlag.USE_EXMNEMONIC2)
+		Set("0f, ae /01", ["FXRSTOR", "", "FXRSTOR64"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag._64BITS | IFlag.PRE_REX | IFlag.USE_EXMNEMONIC2)
 		Set("0f, ae /02", ["LDMXCSR"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS)
 		Set("0f, ae /03", ["STMXCSR"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS)
-		Set("0f, ae /06", ["MFENCE"], [], IFlag.MODRM_REQUIRED | IFlag.MODRR_REQUIRED | IFlag._32BITS)
+
+		# MFENCE and XSAVEOPT share the same opcode 0f ae /6. It's MFENCE when MOD=11, else XSAVEOPT or XSAVEOPT64 in 64.
+		Set("0f, ae /06", ["MFENCE", "XSAVEOPT", "XSAVEOPT64"], [OPT.MEM_OPT], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag.USE_EXMNEMONIC | IFlag.MNEMONIC_MODRM_BASED | IFlag._64BITS | IFlag.PRE_REX | IFlag.USE_EXMNEMONIC2)
 
 		# SFENCE and CLFLUSH share the same opcode 0f ae /7. It's SFENCE when MOD=11, else CLFLUSH.
 		# But the operand is used only for CLFLUSH, which means it's optional. MOD=11 for first mnemonic.
 		Set("0f, ae /07", ["SFENCE", "CLFLUSH"], [OPT.MEM_OPT], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag.USE_EXMNEMONIC | IFlag.MNEMONIC_MODRM_BASED)
-		# Same for LFENCE and XRSTOR with 0f ae /5
-		Set("0f, ae /05", ["LFENCE", "XRSTOR"], [OPT.MEM_OPT], IFlag.MODRM_REQUIRED | IFlag._32BITS |  IFlag.USE_EXMNEMONIC | IFlag.MNEMONIC_MODRM_BASED)
+		# Same for LFENCE and XRSTOR with 0f ae /5.
+		Set("0f, ae /05", ["LFENCE", "XRSTOR", "XRSTOR64"], [OPT.MEM_OPT], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag.USE_EXMNEMONIC | IFlag.MNEMONIC_MODRM_BASED | IFlag._64BITS | IFlag.PRE_REX | IFlag.USE_EXMNEMONIC2)
 
 		Set("0f, af", ["IMUL"], [OPT.REG_FULL, OPT.RM_FULL], IFlag.MODRM_REQUIRED | IFlag._32BITS)
 		Set("0f, b0", ["CMPXCHG"], [OPT.RM8, OPT.REG8], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag.PRE_LOCK)
@@ -471,11 +473,13 @@ class Instructions:
 		# New instructions from Intel 2008:
 		Set("0f, 01, d0", ["XGETBV"], [], IFlag._32BITS)
 		Set("0f, 01, d1", ["XSETBV"], [], IFlag._32BITS)
-		Set("0f, ae /04", ["XAVE"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS)
+		Set("0f, ae /04", ["XSAVE", "", "XSAVE64"], [OPT.MEM], IFlag.MODRM_REQUIRED | IFlag._32BITS | IFlag._64BITS | IFlag.PRE_REX | IFlag.USE_EXMNEMONIC2)
 		# XRSTOR is declared below (see LFENCE), cause it is shared with LFENCE.
 
 		# New instruction from Intel September 2009:
 		Set("0f, 37", ["GETSEC"], [], IFlag.MODRM_REQUIRED | IFlag._32BITS)
+
+		# XSAVEOPT is declared below (see SFENCE).
 
 	def init_FPU(self):
 		Set = lambda *args: self.SetCallback(ISetClass.FPU, *args)
