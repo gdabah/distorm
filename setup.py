a4a75e72ec5a3d72ff9913dda6a3cce5ab6ee49c
Gil Dabah <distorm@gmail.com>
2018-11-10 22:18:12 +0200

    bumping version for pypi

--- setup.py
+++ setup.py
@@ -267,7 +267,7 @@ def main():
 
     # Metadata
     'name'              : 'distorm3',
-    'version'           : '3.3.6',
+    'version'           : '3.3.7',
     'description'       : 'The goal of diStorm3 is to decode x86/AMD64' \
                           ' binary streams and return a structure that' \
                           ' describes each instruction.',
