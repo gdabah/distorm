e8b7ad6ca4aa7c8d208d8c46531c671e5dab8fdf
Gil Dabah <distorm@gmail.com>
2019-12-30 13:47:32 +0200

    updated copyright year

--- src/decoder.h
+++ src/decoder.h
@@ -4,7 +4,7 @@ decoder.h
 diStorm3 - Powerful disassembler for X86/AMD64
 http://ragestorm.net/distorm/
 distorm at gmail dot com
-Copyright (C) 2011  Gil Dabah
+Copyright (C) 2011-2018  Gil Dabah
 This library is licensed under the BSD license. See the file COPYING.
 */
 
