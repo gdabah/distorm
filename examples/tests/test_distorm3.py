ded4d3d850efe4d539216f0219911b6a5d50998c
Aarni Koskela <akx@iki.fi>
2020-01-14 11:42:07 +0200

    Use Registers list from distorm3 module

--- examples/tests/test_distorm3.py
+++ examples/tests/test_distorm3.py
@@ -17,26 +17,14 @@ import distorm3
 YASM_PATH = os.environ.get("YASM_PATH", "yasm")
 
 REG_NONE = 255
-_REGISTERS = ["RAX", "RCX", "RDX", "RBX", "RSP", "RBP", "RSI", "RDI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
-	"EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI", "R8D", "R9D", "R10D", "R11D", "R12D", "R13D", "R14D", "R15D",
-	"AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI", "R8W", "R9W", "R10W", "R11W", "R12W", "R13W", "R14W", "R15W",
-	"AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH", "R8B", "R9B", "R10B", "R11B", "R12B", "R13B", "R14B", "R15B",
-	"SPL", "BPL", "SIL", "DIL",
-	"ES", "CS", "SS", "DS", "FS", "GS",
-	"RIP",
-	"ST0", "ST1", "ST2", "ST3", "ST4", "ST5", "ST6", "ST7",
-	"MM0", "MM1", "MM2", "MM3", "MM4", "MM5", "MM6", "MM7",
-	"XMM0", "XMM1", "XMM2", "XMM3", "XMM4", "XMM5", "XMM6", "XMM7", "XMM8", "XMM9", "XMM10", "XMM11", "XMM12", "XMM13", "XMM14", "XMM15",
-	"YMM0", "YMM1", "YMM2", "YMM3", "YMM4", "YMM5", "YMM6", "YMM7", "YMM8", "YMM9", "YMM10", "YMM11", "YMM12", "YMM13", "YMM14", "YMM15",
-	"CR0", "", "CR2", "CR3", "CR4", "", "", "", "CR8",
-	"DR0", "DR1", "DR2", "DR3", "", "", "DR6", "DR7"]
 
-class Registers(object):
+class _Registers(object):
 	def __init__(self):
-		for i in enumerate(_REGISTERS):
-			if len(i[1]):
-				setattr(self, i[1], i[0])
-Regs = Registers()
+		for index, name in enumerate(distorm3.Registers):
+			if name:
+				setattr(_Registers, name, index)
+
+Regs = _Registers()
 fbin = []
 
 def Assemble(text, mode):
