gdabah/distorm

commit 7745a7c9fca40b9e7acb26e162f4e5546496710c

fixed makefile and warning for linux/gcc

src/config.h
@@ -57,7 +57,7 @@ This library is licensed under the BSD license. See the file COPYING.
 
 #define _DLLEXPORT_
 #define _FASTCALL_
-#define _INLINE_ static
+#define _INLINE_ __inline__
 /* GCC ignores this directive... */
 /*#define _FASTCALL_ __attribute__((__fastcall__))*/
 

make/linux/Makefile
@@ -3,7 +3,7 @@
 #
 
 TARGET_BASE	= libdistorm3.so
-COBJS	= ../../src/mnemonics.o ../../src/wstring.o ../../src/textdefs.o ../../src/prefix.o ../../src/operands.o ../../src/insts.o ../../src/instructions.o ../../src/distorm.o ../../src/decoder.o
+COBJS	= ../../src/mnemonics.o ../../src/textdefs.o ../../src/prefix.o ../../src/operands.o ../../src/insts.o ../../src/instructions.o ../../src/distorm.o ../../src/decoder.o
 CC	= gcc
 CFLAGS	+= -fPIC -O2 -Wall -DSUPPORT_64BIT_OFFSET -DDISTORM_STATIC
 LDFLAGS	+= -shared

make/mac/Makefile
@@ -5,7 +5,7 @@
 DISTORM_MODE ?= DISTORM_DYNAMIC
 TARGET	= libdistorm3.dylib
 PYTHON_BUILD_DIR = ../../Python/macosx-x86
-COBJS	= ../../src/mnemonics.o ../../src/wstring.o ../../src/textdefs.o ../../src/prefix.o ../../src/operands.o ../../src/insts.o ../../src/instructions.o ../../src/distorm.o ../../src/decoder.o
+COBJS	= ../../src/mnemonics.o ../../src/textdefs.o ../../src/prefix.o ../../src/operands.o ../../src/insts.o ../../src/instructions.o ../../src/distorm.o ../../src/decoder.o
 CC	= gcc
 CFLAGS	= -arch i386 -arch x86_64 -O2 -Wall -fPIC -DSUPPORT_64BIT_OFFSET -D${DISTORM_MODE}
 

src/distorm.c
@@ -93,9 +93,9 @@ static void distorm_format_size(unsigned char** str, const _DInst* di, int opNum
 
 	if (isSizingRequired)
 	{
+		/*case 0: break; OT_MEM's unknown size. */
 		switch (di->ops[opNum].size / 8)
 		{
-			/*case 0: break; /* OT_MEM's unknown size. */
 			case 1: strcat_WSN(str, "BYTE "); break;
 			case 2: strcat_WSN(str, "WORD "); break;
 			case 4: strcat_WSN(str, "DWORD "); break;
@@ -103,7 +103,6 @@ static void distorm_format_size(unsigned char** str, const _DInst* di, int opNum
 			case 10: strcat_WSN(str, "TBYTE "); break;
 			case 16: strcat_WSN(str, "DQWORD "); break;
 			case 32: strcat_WSN(str, "YWORD "); break;
-			/*default: /* Big oh uh if it gets here. */ break;
 		}
 	}
 }
@@ -140,7 +139,7 @@ static void distorm_format_signed_disp(unsigned char** str, const _DInst* di, ui
 	else if (ci->features & DF_MAXIMUM_ADDR16) addrMask = 0xffff;
 
 	/* Gotta have full address for (di->addr - ci->codeOffset) to work in all modes. */
-	str_hex(&result->instructionHex, (const char*)&ci->code[(unsigned int)(di->addr - ci->codeOffset)], di->size);
+	str_hex(&result->instructionHex, (const uint8_t*)&ci->code[(unsigned int)(di->addr - ci->codeOffset)], di->size);
 
 	strfinalize_WS(&result->operands, (unsigned char*)&result->operands.p);
 

src/textdefs.c
@@ -19,7 +19,7 @@ static uint8_t Nibble2ChrTable[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8
 void str_hex(_WString* s, const uint8_t* buf, unsigned int len)
 {
 	/* 256 * 2 : 2 chars per byte value. */
-	static int8_t* TextBTable =
+	static char* TextBTable =
 		"000102030405060708090a0b0c0d0e0f" \
 		"101112131415161718191a1b1c1d1e1f" \
 		"202122232425262728292a2b2c2d2e2f" \
@@ -79,7 +79,7 @@ void str_int_impl(unsigned char** s, uint64_t x)
 		*buf++ = NIBBLE_TO_CHR;
 	} while (shift > 0);
 
-	*s += (size_t)(buf - *s);
+	*s += (size_t)((size_t)buf - (size_t)*s);
 }
 
 #else

commit 75821b7a95949fddcc253b1855ea9cc5477d4805

Fix python module .so file loading error
Fixed error: OSError: /SOMEPATH/_distorm3.so: undefined symbol: operands_set_tsi

Issue introduced in commit 7745a7c9fca40b9e7acb26e162f4e5546496710c

src/config.h
@@ -57,7 +57,7 @@ This library is licensed under the BSD license. See the file COPYING.
 
 #define _DLLEXPORT_
 #define _FASTCALL_
-#define _INLINE_ __inline__
+#define _INLINE_ static
 /* GCC ignores this directive... */
 /*#define _FASTCALL_ __attribute__((__fastcall__))*/
 